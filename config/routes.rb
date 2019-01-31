bdc25ff5f580ea4b8d5c3bbf90745a67914a5401
oğuzhan bayram <oguzhan_1907_bayram@hotmail.com>
2019-01-31 13:08:28 +0300

    implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   get 'users/show'
   get 'users/new'
   root 'static_pages#home'
-  get  'home',     to:'static_pages#home'
+  #get  'home',     to:'static_pages#home'
   get  '/help',    to: 'static_pages#help' 
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  get '/signup',  to: 'user#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
  
 end
--- test/controllers/usersshow_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UsersshowControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
